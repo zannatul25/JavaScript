06a7c017a12dadbb872dab886ca1ec4383524b21
zannatul25 <54600092+zannatul25@users.noreply.github.com>
2021-03-01 21:59:14 -0500

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,14 @@
+// Write a function factorial() that takes a number as an argument and returns the factorial of the number.
+// Write function below
+const factorial = number => {
+  let result = 1;
+  if (number <=1 ){
+    return result;
+  }
+  while (number >=2) {
+    result *= number;
+    number--;
+  }
+  return result;
+};
+console.log(factorial(6));
